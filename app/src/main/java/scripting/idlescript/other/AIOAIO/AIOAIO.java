6bee7a2a5c350136987ec3ce78f02c47950ab02c
dginovker <danielginovker@gmail.com>
2024-02-17 21:22:04 +0900

    Add About tab to the GUI

--- app/src/main/java/scripting/idlescript/other/AIOAIO/AIOAIO.java
+++ app/src/main/java/scripting/idlescript/other/AIOAIO/AIOAIO.java
@@ -19,21 +19,6 @@ public class AIOAIO extends IdleScript {
    */
   public static AIOAIO_State state = new AIOAIO_State();
 
-  /**
-   * Version guidelines -
-   *
-   * <ol>
-   *   Major tick - Something large enough it affects the flow of a regular user
-   * </ol>
-   *
-   * <ol>
-   *   Minor tick - New/big improvements to tasks, skills, UI, or any refactors
-   * </ol>
-   *
-   * <ol>
-   *   Patch tick - Bug fixes
-   * </ol>
-   */
   public static final String VERSION = "1.3.0";
 
   public int start(String[] parameters) {
--- app/src/main/java/scripting/idlescript/other/AIOAIO/core/gui/AIOAIO_GUI.java
+++ app/src/main/java/scripting/idlescript/other/AIOAIO/core/gui/AIOAIO_GUI.java
@@ -1,55 +1,32 @@
 package scripting.idlescript.other.AIOAIO.core.gui;
 
-import bot.Main;
 import java.awt.*;
 import javax.swing.*;
 import scripting.idlescript.other.AIOAIO.AIOAIO;
-import scripting.idlescript.other.AIOAIO.core.AIOAIO_Skill;
 
 public class AIOAIO_GUI {
   static JFrame scriptFrame = null;
-  static JList<AIOAIO_Skill> skillList;
-  static DefaultListModel<AIOAIO_Skill> skillListModel;
-  static TasksPanel tasksPanel; // Use the new TasksPanel class
-  static JButton startButton;
+  static JTabbedPane tabbedPane;
 
   public static void setupGUI() {
     scriptFrame = new JFrame("AIO AIO v" + AIOAIO.VERSION);
     scriptFrame.setLayout(new BorderLayout());
-    skillListModel = new DefaultListModel<>();
-    AIOAIO.state.botConfig.skills.forEach(skillListModel::addElement);
-    skillList = new JList<>(skillListModel);
-    skillList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-    skillList.addListSelectionListener(
-        e -> {
-          if (!e.getValueIsAdjusting()) {
-            tasksPanel.updatePanel(); // Use the updatePanel method from TasksPanel
-          }
-        });
-    tasksPanel = new TasksPanel(skillList, skillListModel);
-    startButton = new JButton("Start");
-    startButton.addActionListener(
-        e -> {
-          scriptFrame.dispose();
-          AIOAIO.state.botConfig.saveConfig();
-          AIOAIO.state.startPressed = true;
-          Main.getController().log("AIO AIO starting!");
-        });
-    JPanel skillsPanel = createSectionWithTitle("Skills", new JScrollPane(skillList));
-    scriptFrame.add(skillsPanel, BorderLayout.WEST);
-    scriptFrame.add(createSectionWithTitle("Tasks", tasksPanel), BorderLayout.CENTER);
-    scriptFrame.add(startButton, BorderLayout.SOUTH);
+
+    tabbedPane = new JTabbedPane();
+
+    // Adding Tasks Panel
+    Tasks_GUI tasksPanel = new Tasks_GUI();
+    tabbedPane.addTab("Tasks", tasksPanel.getPanel());
+
+    // Adding About Panel
+    About_GUI aboutPanel = new About_GUI();
+    tabbedPane.addTab("About", aboutPanel.getPanel());
+
+    scriptFrame.add(tabbedPane, BorderLayout.CENTER);
+
     scriptFrame.pack();
     scriptFrame.setLocationRelativeTo(null);
     scriptFrame.setVisible(true);
     scriptFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  }
-
-  private static JPanel createSectionWithTitle(String title, Component component) {
-    JPanel sectionPanel = new JPanel(new BorderLayout());
-    JLabel titleLabel = new JLabel(title, SwingConstants.CENTER);
-    sectionPanel.add(titleLabel, BorderLayout.NORTH);
-    sectionPanel.add(component, BorderLayout.CENTER);
-    return sectionPanel;
   }
 }
